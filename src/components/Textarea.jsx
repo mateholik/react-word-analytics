0fec89d8c70ee717aa4d6ad00ec6469e2d1c97df
Vladimir Vaisov <vaisov.vladimir@gmail.com>
2025-06-03 17:16:08 +0300

    basic validation

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -1,3 +1,33 @@
+import { useState } from 'react';
+import Warning from './Warning';
+
 export default function Textarea() {
-  return <textarea className='textarea' />;
+  const [value, setValue] = useState('');
+  const [warningText, setWarningText] = useState('');
+  const handleChange = (event) => {
+    let newValue = event.target.value;
+
+    if (newValue.includes('<script>')) {
+      setWarningText('Script tags are not allowed.');
+      newValue = newValue.replace(/<script>/g, '');
+    } else if (newValue.includes('@')) {
+      setWarningText('@ symbol is not allowed.');
+      newValue = newValue.replace(/@/g, '');
+    } else {
+      setWarningText('');
+    }
+
+    setValue(newValue);
+  };
+  return (
+    <div className='textarea'>
+      <textarea
+        placeholder='Enter your text'
+        value={value}
+        onChange={handleChange}
+        spellCheck='false'
+      />
+      {warningText && <Warning warningText={warningText} />}
+    </div>
+  );
 }
