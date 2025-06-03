d6282ec9f47362b4f7aa2d9ebd21934d64bd2f70
Vladimir Vaisov <vaisov.vladimir@gmail.com>
2025-06-03 16:30:20 +0300

    add structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className='footer'>
+      <small>&copy; Copyright by vladis.lt</small>
+      <small>Last checked limits</small>
+    </footer>
+  );
+}
